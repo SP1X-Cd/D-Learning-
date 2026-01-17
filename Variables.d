5dbc02d33d527ac745643030ac8c87e553fcfcf0
SP1X-Cd <belowigor753@gmail.com>
2026-01-17 12:44:21 +0600

    Add Variables.d with basic variable declarations

---
+++ Variables.d
@@ -0,0 +1,10 @@
+import std.stdio;
+
+void main() {
+  byte a; //8 bytes
+  int b; //32 bytes
+  double c; //Number with dot
+  float d = 5.37f; //"f" = float
+  size_t e; //MAXIMUM
+  /*This is the base, but there is more.*/
+}
